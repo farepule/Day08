25bdbfed9e25fbe232bc7e6abc71c94c46f1b31d
farepule <faruk.puska@gmail.com>
2021-03-16 06:56:21 +0100

    EX08

---
+++ ex08/src/index.js
@@ -0,0 +1,13 @@
+//Only change code below this line
+var total = 0;
+function funcWithArg(digit) {
+    
+    var dig = (digit + 9) / 5;
+    return dig;
+
+
+}
+total = funcWithArg(6)
+//Only change code above this line
+console.log(total);
+module.exports = funcWithArg;
